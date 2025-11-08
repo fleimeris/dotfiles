20a2c256ae111a22ca5991f3c348055037adb23f
Fleimeris <44847763+fleimeris@users.noreply.github.com>
2025-11-08 12:11:58 +0200

    Initial commit

---
+++ create_sym_links.sh
@@ -0,0 +1,15 @@
+#!/bin/fish
+
+set PROJECT_DIR $(pwd)
+
+ln -s -f $PROJECT_DIR/emacs/.emacs ~/.emacs
+
+mkdir -p ~/.config/hypr
+ln -s -f $PROJECT_DIR/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+
+mkdir -p ~/.config/hyprpaper/
+ln -s -f $PROJECT_DIR/hyprpaper/hyprpaper.conf ~/.config/hyprpaper/hyprpaper.conf
+
+mkdir -p ~/.config/waybar
+ln -s -f $PROJECT_DIR/waybar/config.jsonc ~/.config/waybar/config.jsonc
+ln -s -f $PROJECT_DIR/waybar/style.css ~/.config/waybar/style.css
