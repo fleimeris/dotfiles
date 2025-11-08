8752771e65c95cb797e8bd4c207bfa0dc166368d
Fleimeris <44847763+fleimeris@users.noreply.github.com>
2025-11-08 22:51:43 +0200

    Wallpapers

---
+++ install_software.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo dnf update -y
+sudo dnf copr enable -y solopasha/hyprland
+sudo dnf install Hyprland xdg-desktop-portal-hyprland dunst waybar hyprlauncher hyprpaper fish swww
---
+++ setup-wallpapers.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+
+set PROJECT_DIR $(pwd)
+
+mkdir -p ~/Pictures/Wallpapers
+
+ln -s -f $PROJECT_DIR/wallpapers/1.jpg ~/Pictures/Wallpapers/1.jpg
